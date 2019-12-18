850087018302e3caaafef775aaf9a81a9539bcc9
Md Rahman <mdrahman@Mds-MacBook-Pro.local>
2019-12-17 22:18:16 -0500

    adding arfan

---
+++ arfan.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Your life purpose consists of the central motivating aims of your life—the reasons you get up in the morning. Purpose can guide life decisions, influence behavior, shape goals, offer a sense of direction, and create meaning. For some people, purpose is connected to vocation—meaningful, satisfying work. " > output.txt
